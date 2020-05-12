05dff015f5889b6517ffba7dae24372dfec7691d
Alin <JiangLinChen94@163.com>
2020-05-12 10:04:45 +0800

    使用数据源的方式对数据进行优化

--- test_cases/login_suit/login_case.py
+++ test_cases/login_suit/login_case.py
@@ -6,7 +6,7 @@ from Action.login_action import LoginAction
 from page.login_page.zentao_login_page import ZenTaoLoginPage
 from page.main_page.zentao_main_page import ZenTaoMainPage
 from common.selenium_base_case import SeleniumBaseCase
-
+from common.test_data_utils import TestDataUtils
 
 class TestZenTaoLogin(SeleniumBaseCase):
 
@@ -15,20 +15,25 @@ class TestZenTaoLogin(SeleniumBaseCase):
     #     self.base_page = BasePage(driver)
     #     self.main_page = ZenTaoMainPage(driver)
     #     self.base_page.set_driver()
+    test_class_data = TestDataUtils('login_suit', 'TestZenTaoLogin').convert_excel_data_to_test_data()
 
     def test_login_success(self):
         """
         禅道登录
         """
+        test_function_data = self.test_class_data['test_login_success']
+        self._testMethodDoc = test_function_data['test_name']
         login_action = LoginAction(self.base_page.driver)
-        main_page = login_action.login_success('admin', 'alin19941226061X')
+        main_page = login_action.login_success(test_function_data['test_parameter'].get('username'), test_function_data['test_parameter'].get('password'))
         actual_result = main_page.get_username()
-        self.assertEqual(actual_result, 'admin', 'test_login_success用例执行失败')
+        self.assertEqual(actual_result, test_function_data['expected_result'], test_function_data['test_message'])
 
     def test_login_fail(self):
+        test_function_data = self.test_class_data['test_login_fail']
+        self._testMethodDoc = test_function_data['test_name']
         login_action = LoginAction(self.base_page.driver)
-        alert_value = login_action.login_fail('test', 'alin199')
-        self.assertEqual(alert_value, '登录失败，请检查您的用户名或密码是否填写正确。', 'test_login_fail用例执行失败')
+        alert_value = login_action.login_fail(test_function_data['test_parameter'].get('username'), test_function_data['test_parameter'].get('password'))
+        self.assertEqual(alert_value, test_function_data['expected_result'], test_function_data['test_message'])
 
     def tearDown(self) -> None:
         self.base_page.quit_browser()
